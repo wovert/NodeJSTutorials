c45d1171eade586e962adcd50e43868b5c4cad13
wovet <wovert@126.com>
2018-04-19 12:15:14 +0800

    ejs template

--- 06-template/ejs.js
+++ 06-template/ejs.js
@@ -1,6 +1,22 @@
 const ejs = require('ejs');
 
-var param = {name: 'wovert'};
+var param = {
+name: 'wovert', 
+type: 'admin',
+json: { arr:  [
+    {
+        'user': 'blue',
+        'pwd': 'bluepw'
+    },
+    {
+        'user': 'green',
+        'pwd': 'greenpw'
+    },
+    {
+        'user': 'redb',
+        'pwd': 'redpw'
+    }]
+}};
 var str = ejs.renderFile('./views/1.ejs', param, function(err, data){
     if(err) {
         console.log('Compile error!')
